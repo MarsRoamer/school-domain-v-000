9f9f68102e38de519740938801ec0d3fbe7c1c7b
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-05 16:47:14 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,12 +19,12 @@ end
 def add_student(student_name, grade)
 
   if grade == 9
-    @roster[:9] << student_name
+    @roster[9] << student_name
   elsif grade == 10
-    @roster[:10] << student_name
+    @roster[10] << student_name
   elsif grade == 11
-    @roster[:11] << student_name
+    @roster[11] << student_name
   elsif grade == 12
-    @roster[:12] << student_name
+    @roster[12] << student_name
   end
 end
