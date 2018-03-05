7afb9e1e5563e372c3dff5a30f1d770199f36b02
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-05 16:20:02 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,11 +2,11 @@ class School
 
 def initialize (name)
   @name == name
+  roster = {}
+  @roster = roster
 end
 
-def roster
-  @roster = {}
-end
+
 
 def add_student(student_name, grade)
   @roster = roster[:grade] = student_name
